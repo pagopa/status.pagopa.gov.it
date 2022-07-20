7e30e5664e914b8e998d297a5f3d3daa61138308
Vito Falco <vitofalco@gmail.com>
2022-07-20 13:00:56 +0200

    FXI & refact

--- main.js
+++ main.js
@@ -1,35 +1,47 @@
 (function() {
-    const dictURL = "https://raw.githubusercontent.com/pagopa/status.pagopa.gov.it/main/main.js";
+    const dictURL = "https://raw.githubusercontent.com/pagopa/status.pagopa.gov.it/main/dict.json";
     // OVERRIDE THE MONTHS DICT
-    window.monthStrings = ["Gen", "Feb", "Mar", "Apr", "Mag", "Giu", "Lug", "Ago", "Set", "Ott", "Nov", "Dic"]
+    window.monthStrings = ["Gen", "Feb", "Mar", "Apr", "Mag", "Giu", "Lug", "Ago", "Set", "Ott", "Nov", "Dic"];
 
-    const dict = await fetch(dictURL, {
+    function translateDateList() {
+        const days = document.querySelectorAll(".status-day .date");
+        days.forEach( day => {
+            const dayInTS = Date.parse( day.textContent );
+            const formattedDay = new Intl.DateTimeFormat('it-IT').format(dayInTS);
+            day.innerHTML = formattedDay;
+        })
+    }
+
+    function dictManage(dict) {
+        dict.forEach( (element) => {
+            let occurs = document.querySelectorAll(element.selector);
+            occurs.forEach( (occ) => {
+                // if we want to replace a piece of text
+                if (element.replaceString) {
+                    let occHTML = occ.innerHTML;
+                    occHTML = occHTML.replace(element.replaceString, element.string);
+                    occ.innerHTML = occHTML;
+                } else {
+                    // if we want replace text entirely
+                    occ.textContent = element.string;
+                }
+            })
+        });
+    }
+
+    // obtain DICT & manipulate nodes
+    fetch(dictURL, {
         headers: {
-          'Accept': 'application/json'
-        }
-    }).then((resp)=> {
-        if (response.ok) {
-            return response.json();
+        "Accept": "application/json"
         }
     })
-    .catch((error) => {
-        console.error("DICT URL FAILED");
-        return [];
-    });
+    .then( response => response.ok && response.json() )
+    .then( data => {
+        dictManage(data);
+    })
+    .catch( error => console.log("FAIL TO LOAD DICT") );
 
-    dict.forEach( (element) => {
-        let occurs = document.querySelectorAll(element.selector);
-        occurs.forEach( (occ) => {
-            // if we want to replace a piece of text
-            if (element.replaceString) {
-                let occHTML = occ.innerHTML;
-                occHTML = occHTML.replace(element.replaceString, element.string);
-                occ.innerHTML = occHTML;
-            } else {
-                // if we want replace text entirely
-                occ.textContent = element.string;
-            }
-        })
-    });
+    // translate date in list
+    translateDateList();
 
 })();
